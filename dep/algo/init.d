831d8284a9b6dc2a28e7b669a7bca0d7f578b4b2
crytalsith <crystalsithh@gmail.com>
2024-12-09 16:58:32 +0100

    texture ok

--- dep/algo/init.d
+++
@@ -1,6 +0,0 @@
-obj/algo/init.o: src/algo/init.c src/algo/../../includes/cub3d.h \
- src/algo/../../includes/../mlx/mlx.h \
- src/algo/../../includes/../libft/libft.h
-src/algo/../../includes/cub3d.h:
-src/algo/../../includes/../mlx/mlx.h:
-src/algo/../../includes/../libft/libft.h:
--- dep/map/flood_fill.d
+++
@@ -1,6 +0,0 @@
-obj/map/flood_fill.o: src/map/flood_fill.c src/map/../../includes/cub3d.h \
- src/map/../../includes/../mlx/mlx.h \
- src/map/../../includes/../libft/libft.h
-src/map/../../includes/cub3d.h:
-src/map/../../includes/../mlx/mlx.h:
-src/map/../../includes/../libft/libft.h:
