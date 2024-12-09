8e24435a50088d5c3e074a16d696bdf6404a128e
crytalsith <crystalsithh@gmail.com>
2024-12-09 13:22:29 +0100

    merge raycast

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -10,10 +10,10 @@
 # define MAP_WIDTH 10
 # define PI 3.14159265359
 
-# define RGBRED 0xFF0000
-# define RGBGREEN 0x00FF00
-# define RGBBLUE 0x0000FF
-# define RGBDEFAULT 0xFFFF00 // Jaune
+#define rgbRED 0xFF0000
+#define rgbGREEN 0x00FF00
+#define rgbBLUE 0x0000FF
+#define rgbDEFAULT 0xFFFF00 // Jaune
 
 
 # include "../mlx/mlx.h"
@@ -28,43 +28,48 @@
 # include <stdbool.h>
 # include <math.h>
 
-// typedef struct s_colorRGB
-// {
-//    int  r;
-//    int  g;
-//    int  b;
-// } t_colorRGB;
-
 typedef struct s_ray
 {
-	double	rayDirX;
-	double	rayDirY;
-	double	planeX;
-	double	planeY;
-	double	deltaDistX; // Distance to go through 1 case
-	double	deltaDistY;
-	double	sideDistX;
-	double	sideDistY;
-	double	perpWallDist;
-	int		dirX;
-	int		dirY;
-	int		stepX; // Step direction (-1 or 1)
-	int		stepY;
-	int		hit; // Hit a wall
-	int		side; // Where the wall is hit
-	int		FOV; // Field of view
-}			t_ray;
+	double  rayDirX;
+	double  rayDirY;
+	double  planeX;
+	double  planeY;
+	double  deltaDistX; // Distance to go through 1 case
+	double  deltaDistY;
+	double  sideDistX;
+	double  sideDistY;
+	double  posX; // Position of the ray
+	double  posY;
+	double  perpWallDist;
+	double  time;
+	double  oldTime;
+	double     dirX;
+	double     dirY;
+	int     stepX; // Step direction (-1 or 1)
+	int     stepY;
+	int     hit; // Hit a wall
+	int     side; // Where the wall is hit
+} t_ray;
+
+typedef struct s_key
+{
+	int     forward;
+	int     backward;
+	int     turn_left;
+	int     turn_right;
+	int     escape;
+} t_key;
 
 typedef struct s_player
 {
-	double	x; // Player EXACT position
-	double	y;
-	int		mapX; // Player position (1 or 0)
-	int		mapY;
-	double	old_x; // Player previous exact position
-	double	old_y;
-	double	angle; // Player angle
-}			t_player;
+	double  x; // Player EXACT position
+	double  y;
+	int     mapX; // Player position (1 or 0)
+	int     mapY;
+	double  old_x; // Player previous exact position
+	double  old_y;
+	double  angle; // Player angle
+} t_player;
 
 
 typedef struct s_game_data
@@ -93,21 +98,26 @@ typedef struct s_game_data
 	char		*player_addr;
 	int			endian;
 	int			line_length;
+	t_key		key;
 	t_ray		raycast;
 	t_player	player;
 }				t_game_data;
 
 //Algo
-void	image_init(t_game_data *game);
-void	draw_map(t_game_data *game);
+void    image_init(t_game_data *game);
+void    draw_map(t_game_data *game);
 void	my_mlx_pixel_put(t_game_data *data, int x, int y, int color);
-void	game_data_init(t_game_data *game);
-int		render_frame(t_game_data *game);
-void	draw_player(t_game_data *game, double x, double y, int radius, int color);
-void	raycasting(t_game_data *game);
-void	digital_differential_analyzer(t_game_data *game, int x);
-void	init_game(t_game_data *game);
-
+void    game_data_init(t_game_data *game);
+int     render_frame(t_game_data *game);
+void    draw_player(t_game_data *game, double x, double y, int radius, int color);
+void    raycasting(t_game_data *game);
+void    digital_differential_analyzer(t_game_data *game, int x);
+void    move_back(t_game_data *game, double moveSpeed);
+void    move_front(t_game_data *game, double moveSpeed);
+void    turn_right(t_game_data *game, double rotation_speed);
+void	turn_left(t_game_data *game, double rotation_speed);
+void 	update_player(t_game_data *game);
+void    init_game(t_game_data *game);
 //Exec
 int		map_check(char **input);
 int		check_lines(char **map);
--- src/algo/init.c
+++ src/algo/init.c
@@ -11,19 +11,32 @@ void	image_init(t_game_data *game)
 	// game->player_addr = mlx_get_data_addr(game->player_img, &game->bpp, &game->line_length, &game->endian);
 }
 
-void	init_game(t_game_data *game)
+void    init_game(t_game_data *game)
 {
-	game->player.old_x = 0; // Player previous position
-	game->player.old_y = 0;
-	game->player.angle = 0; // Player angle
-	game->raycast.dirX = -1; // Initial vector direction
-	game->raycast.dirY = 0;
-	game->raycast.planeX = 0; // 2D raycaster of camera plane
-	game->raycast.planeY = 0.66;
-	game->raycast.deltaDistX = 0;
-	game->raycast.deltaDistY = 0;
-	game->raycast.sideDistX = 0;
-	game->raycast.sideDistY = 0;
-	game->raycast.perpWallDist = 0;
-	game->raycast.hit = 0;
+    game->player.old_x = 0; // Player previous position
+    game->player.old_y = 0;
+    game->raycast.dirX = -1.0; // Initial vector direction
+    game->raycast.dirY = 0.0;
+    game->raycast.planeX = 0; // 2D raycaster of camera plane
+    game->raycast.planeY = 0.66; 
+    game->raycast.deltaDistX = 0;
+    game->raycast.deltaDistY = 0;
+    game->raycast.sideDistX = 0;
+    game->raycast.sideDistY = 0;
+    game->raycast.perpWallDist = 0;
+    game->raycast.hit = 0;
+    game->raycast.stepX = 0;
+    game->raycast.stepY = 0;
+    game->raycast.side = 0;
+    game->raycast.time = 0;
+    game->raycast.oldTime = 0;
+    game->raycast.rayDirX = 0;
+    game->raycast.rayDirY = 0;
+    game->key.forward = 0;
+    game->key.backward = 0;
+    game->key.turn_left = 0;
+    game->key.turn_right = 0;
+    game->key.escape = 0;
+    printf("dirX: %.2f, dirY: %.2f\n", game->raycast.dirX, game->raycast.dirY);
+
 }
