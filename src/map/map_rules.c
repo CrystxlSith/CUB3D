dd61fe920350710c251accf17143b5e4612647d2
Canybardeloton <armel.gilibert@gmail.com>
2024-12-17 14:18:25 +0100

    Wall check fixed

--- src/map/map_rules.c
+++ src/map/map_rules.c
@@ -5,11 +5,19 @@ int	check_walls(int width, int index, char **map)
 	int	i;
 
 	i = 0;
-	while (map[index][i] == '0' || map[index][i] == '1'
-		|| map[index][i] == 'N' || map[index][i] == 'S'
-		|| map[index][i] == 'E' || map[index][i] == 'W'
-		|| map[index][i] == ' ' || map[index][i] == 'P')
-		i++;
+	if (index == 0 || map[index + 1] == NULL)
+	{
+		while (map[index][i] == ' ' || map[index][i] == '1')
+			i++;
+	}
+	else
+	{
+		while (map[index][i] == '0' || map[index][i] == '1'
+			|| map[index][i] == 'N' || map[index][i] == 'S'
+			|| map[index][i] == 'E' || map[index][i] == 'W'
+			|| map[index][i] == ' ' || map[index][i] == 'P')
+			i++;
+	}
 	if (i == width && map[index][i - 1] == '1')
 		return (TRUE);
 	else
